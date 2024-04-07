2241fa04f936092e45e45ca8ef6130e0859ccadd
Dylan Barks <dylan.barks95@gmail.com>
2024-04-07 17:36:30 -0600

    fields, constructor, getters, setters

--- Grocery_List_Project/src/groceryList/GroceryItem.java
+++ Grocery_List_Project/src/groceryList/GroceryItem.java
@@ -1,5 +1,31 @@
 package groceryList;
 
-public class GroceryItem {
 
-}
+/**
+ * Class Grocery Item
+ */
+public class GroceryItem {
+	String name;
+	String brand;
+	/**
+	 * @param name
+	 * @param brand
+	 */
+	public GroceryItem(String name, String brand) {
+		this.name = name;
+		this.brand = brand;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+} // 
