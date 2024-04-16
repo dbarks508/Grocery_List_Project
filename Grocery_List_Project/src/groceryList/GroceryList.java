0995649a22180a5c352986b39f5cbc6bae7c6de5
Dylan Barks <dylan.barks95@gmail.com>
2024-04-15 19:26:27 -0600

    added price class and funtionality to GroceryList to read in from a file and generate a list of price objects

--- Grocery_List_Project/src/groceryList/GroceryList.java
+++ Grocery_List_Project/src/groceryList/GroceryList.java
@@ -1,15 +1,19 @@
 package groceryList;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Class GroceryList
  */
 public class GroceryList {
 	List<GroceryItem> groceryList;
+	String file;
 	
-	public GroceryList(List<GroceryItem> groceryList) {
+	public GroceryList(List<GroceryItem> groceryList, String file) {
 		this.groceryList = groceryList;
+		this.file = file;
 	}
 	
 	public int getNumberOfItems() {
@@ -25,5 +29,30 @@ public class GroceryList {
 		}
 		return numOfVeg;
 	} 
+	
+	public List<Price> generatePriceList() {
+		List<Price> prices = new ArrayList<>();
+		try(Scanner reader = new Scanner(GroceryList.class.getResourceAsStream(file))){
+			while(reader.hasNext()) {
+				String line = reader.nextLine();
+				try{
+					line = line.strip();
+					String[] splitLine = line.split(",");
+				
+					String name = splitLine[0];
+					double p = Double.parseDouble(splitLine[1]);
+					Price price = new Price(name, p);
+					prices.add(price);
+				} catch (IllegalArgumentException | ArrayIndexOutOfBoundsException e) {
+					System.err.println("Error: " + line);
+				}
+			} // end while 
+		}// end outer try
+		return prices;
+	} // end method
+	
+	public double estimatePrice() {
+		return 0.0;
+	}
 	
 } // end class
