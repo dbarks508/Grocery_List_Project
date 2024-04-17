5f27e2fd7d9f245aab5284d6d7e2784f25e5a8f3
Dylan Barks <dylan.barks95@gmail.com>
2024-04-17 15:30:46 -0600

    estimatePrice class plus a few more items in Prices.csv. Tested in GroceryApp

--- Grocery_List_Project/src/groceryList/GroceryList.java
+++ Grocery_List_Project/src/groceryList/GroceryList.java
@@ -1,5 +1,6 @@
 package groceryList;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -30,6 +31,10 @@ public class GroceryList {
 		return numOfVeg;
 	} 
 	
+	/**
+	 * Method returns a list of price objects based on the Prices file
+	 * @return List<Price>   list of price objects created from Prices file 
+	 */
 	public List<Price> generatePriceList() {
 		List<Price> prices = new ArrayList<>();
 		try(Scanner reader = new Scanner(GroceryList.class.getResourceAsStream(file))){
@@ -51,8 +56,26 @@ public class GroceryList {
 		return prices;
 	} // end method
 	
-	public double estimatePrice() {
-		return 0.0;
-	}
+	/**
+	 * Method estimates the total price of the groceryList based off of the prices listed in 
+	 * the Prices file
+	 * @return double    estimated price of the groceryList
+	 */
+	public String estimatePrice() {
+		List<Price> prices = this.generatePriceList();
+		double total = 0;
+		
+		for(int i = 0; i < prices.size(); i++) {
+			Price price = prices.get(i); 
+			for (GroceryItem g: groceryList) {
+				if (g.name.equals(price.name)) {
+					total += price.price;
+				}
+			} // end inner
+		} // end outer
+		DecimalFormat df = new DecimalFormat("#.##");
+		String roundedValue = df.format(total);
+		return "$" + roundedValue;
+	} // end method
 	
 } // end class
