1eaae2590c8aa650ad86207291e3782b2755e0e5
Dylan Barks <dylan.barks95@gmail.com>
2024-04-21 14:59:00 -0600

    added doc comments

--- Grocery_List_Project/src/groceryList/SnackItem.java
+++ Grocery_List_Project/src/groceryList/SnackItem.java
@@ -1,21 +1,42 @@
 package groceryList;
 
+/**
+ * class SnackITem extends GroceryItem
+ */
 public class SnackItem extends GroceryItem{
 	Size size;
 
+	/**
+	 * Constructor initializes fields
+	 * @param name    name of item
+	 * @param brand   brand of item
+	 * @param size    size of item
+	 */
 	public SnackItem(String name, String brand, Size size) {
 		super(name, brand);
 		this.size = size;
 	} // end constructor
 
+	/**
+	 * gets size
+	 * @return size 
+	 */
 	public Size getSize() {
 		return size;
 	}
 
+	/**
+	 * Sets size
+	 * @param size set to this size
+	 */
 	public void setSize(Size size) {
 		this.size = size;
 	}
 	
+	/**
+	 * Method returns string comprised of name, brand and size of SnackItem
+	 * @return formatted string 
+	 */
 	@Override
 	public String toString() {
 		return String.format("%s (%s size)", super.toString(), 
